769f50fce26d958f2e21a0fb61552a76e4af3dbd
Chris Beaumont <beaumont@hawaii.edu>
2013-06-24 10:57:07 -0400

    inital commit

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mkdir build
+cd build
+which qmake
+cmake \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_RPATH=$LD_RUN_PATH \
+    -DPYTHON_EXECUTABLE=$PYTHON \
+    -DLIB_INSTALL_DIR=$PREFIX/lib \
+    -DShiboken_DIR=$PREFIX \
+    -DPySide_DIR=$PREFIX \
+    -DQT_MOC_EXECUTABLE=/usr/bin/moc \  #hack -- these aren't in anaconda yet
+    -DQT_RCC_EXECUTABLE=/usr/bin/rcc \
+    -DQT_UIC_EXECUTABLE=/usr/bin/uic \
+    ..
+make
+make install
+
+# ----------------------------------------------------------
+
+LIB=$PREFIX/lib
+SP=$LIB/python2.7/site-packages
+
+if [ `uname` == Darwin ]; then
+    cp -r /Library/Python/2.7/site-packages/pysideuic $SP
+    cp /usr/bin/pyside-rcc $PREFIX/bin
+    cp /usr/bin/pyside-uic $PREFIX/bin
+    cp /usr/bin/pyside-lupdate $PREFIX/bin
+fi
---
+++ run_test.py
@@ -0,0 +1,29 @@
+import PySide.QtCore
+import PySide.QtDeclarative
+import PySide.QtGui
+import PySide.QtHelp
+import PySide.QtMultimedia
+import PySide.QtNetwork
+import PySide.QtScript
+import PySide.QtScriptTools
+import PySide.QtSql
+import PySide.QtSvg
+import PySide.QtTest
+import PySide.QtUiTools
+import PySide.QtWebKit
+import PySide.QtXml
+import PySide.QtXmlPatterns
+import pysideuic
+
+import sys
+if sys.platform != 'linux2':
+    import PySide.QtOpenGL
+    import PySide.phonon
+
+from subprocess import check_call, CalledProcessError
+check_call(['pyside-uic', '--version'])
+check_call(['pyside-lupdate', '-version'])
+try:
+    check_call(['pyside-rcc', '-version'])
+except CalledProcessError as exc:
+    assert exc.returncode == 1
